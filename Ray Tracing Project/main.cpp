856aff786749d3e2c1d81f6d3b70c28c7a72e093
Alexander Veloz <alecpoptart97@gmail.com>
2019-01-14 18:56:36 -0800

    Initial Commit

---
+++ Ray Tracing Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ray Tracing Project
+//
+//  Created by Alec Veloz on 1/14/19.
+//  Copyright © 2019 Alec. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
