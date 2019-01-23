f06f8bcda813d1cac6fc6a65cdec5565fe3e99e2
Alexander Veloz <alecpoptart97@gmail.com>
2019-01-23 13:22:39 -0800

    phong_shader.cpp: More cleaning

--- Ray Tracing Project/phong_shader.cpp
+++ Ray Tracing Project/phong_shader.cpp
@@ -11,36 +11,29 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 	vec3 color;
 	
 	//TODO; //determine the color
-	//color = R_a*L_a +R_d*L_dmax(dot(normal,lightvec),0)+R_s*L_s*max(dot(v,reflection)^a;
-	//R = color_<type>; L = light->emitted_light();
+	// color = R_a * L_a + R_d * L_d * max(dot(normal, lightvec), 0) + R_s * L_s * max(dot(v, reflection)^a;
+	// R_<type>   = color_<type>; L = light->emitted_light(Point_to_Light);
+	// lightvec   = light to point/intersection ray; v = point/intersection to viewer direction
+	// reflection = Light reflection direction
 	
 	vec3 ambient_intensity, diffuse_intensity, specular_intensity = {0,0,0};
-	// vec3 k_a = ambient_reflection constant
-	// vec3 i_a = ambient_reflection intensity (color?)
 	// ambient_intensity = color_ambient;
 	for (Light* light : world.lights)
 	{
-		//color += (k_d*i_m_d*(L_m*N) + k_s*i_m_s*(R_m*V)^(a))
-		//vec3 k_d = diffuse reflection constant (lambertian reflectance)
-		//vec3 L_m = direction vector from point on surface towards each light source (light)
-		vec3 Point_to_Light = light->position-intersection_point;
-		//vec3 N = normal at point on surface
-		//vec3 i_m_d = intensity of diffuse component of light source (light)
-		//vec3 k_s = specular reflection constant
-		vec3 Reflected_Direction = (2 * dot(normal,Point_to_Light.normalized()) * normal - Point_to_Light.normalized()).normalized() ; //reflection of L_m
-		//vec3 v = direction pointing towards viewer = -ray.direction
-		vec3 V = (ray.endpoint-intersection_point).normalized();
-		//double a = shiniess constant = specular_power
-		//vec3 i_m_s = intensity of specular component of light source (light)
+		vec3 Intersection_to_Light = light->position-intersection_point;
+		vec3 Light_Produced = light->Emitted_Light(Intersection_to_Light);
+		vec3 reflection = (2 * dot(normal,Intersection_to_Light.normalized()) * normal - Intersection_to_Light.normalized()).normalized();
+		vec3 v = (ray.endpoint-intersection_point).normalized();
 		
-		diffuse_intensity +=  color_diffuse * light->Emitted_Light(Point_to_Light) * fmax(dot(normal, Point_to_Light.normalized()),0.0);
-		specular_intensity += color_specular * light->Emitted_Light(Point_to_Light) * pow(fmax(dot(V, Reflected_Direction), 0.0),specular_power);
 		
+		
+		diffuse_intensity +=  color_diffuse  * Light_Produced * fmax(dot(normal, Intersection_to_Light.normalized()), 0.0);
+		specular_intensity += color_specular * Light_Produced * pow(fmax(dot(v, reflection), 0.0), specular_power);
 	}
 	color = ambient_intensity + diffuse_intensity + specular_intensity;
 	if(debug_pixel)
 	{
-		std::cout<<"call Shade_Surface with location = "<< intersection_point<<"; normal = "<<normal<<std::endl<<"ambient = "<<color_ambient<<std::endl;
+		std::cout<<"call Shade_Surface with location = "<<intersection_point<<"; normal = "<<normal<<std::endl<<"ambient = "<<color_ambient<<std::endl;
 		std::cout<<"shading for light: diffuse = "<<diffuse_intensity<<"; specular = "<<specular_intensity<<std::endl<<"final color "<<color<<std::endl;
 	}
 	return color;
