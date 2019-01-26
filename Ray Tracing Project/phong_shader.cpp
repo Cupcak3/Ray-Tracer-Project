d992f7474569b3b78805a43eb24cb3e84eadd333
Alexander Veloz <alecpoptart97@gmail.com>
2019-01-25 23:35:32 -0800

    phong_shader.cpp: Cleaning

--- Ray Tracing Project/phong_shader.cpp
+++ Ray Tracing Project/phong_shader.cpp
@@ -12,19 +12,20 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 	
 	// TODO; //determine the color
 	// color = R_a * L_a + R_d * L_d * max(dot(normal, lightvec), 0) + R_s * L_s * max(dot(v, reflection)^a;
-	// R_<type>   = color_<type>; L = light->emitted_light(Point_to_Light);
+	// R_<type>   = color_<type>; L_d = L_s = light->emitted_light(Point_to_Light);
 	// lightvec   = light to point/intersection ray; v = point/intersection to viewer direction
 	// reflection = Light reflection direction
 	
 	vec3 ambient_intensity, diffuse_intensity, specular_intensity = {0,0,0};
+	
 	ambient_intensity = color_ambient * world.ambient_color * world.ambient_intensity;
+	
 	for (Light* light : world.lights)
 	{
 		vec3 Intersection_To_Light = light->position-intersection_point;
 		vec3 Light_Produced = light->Emitted_Light(Intersection_To_Light);
 		vec3 Reflected_Ray = (2 * dot(normal,Intersection_To_Light.normalized()) * normal - Intersection_To_Light.normalized()).normalized();
 		vec3 View_Ray = -(ray.direction).normalized();
-		
 		
 		if (!world.enable_shadows)
 		{
@@ -44,11 +45,14 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 			}
 		}
 	}
+	
 	color = ambient_intensity + diffuse_intensity + specular_intensity;
+	
 	if(debug_pixel)
 	{
 		std::cout<<"call Shade_Surface with location = "<<intersection_point<<"; normal = "<<normal<<std::endl<<"ambient = "<<color_ambient<<std::endl;
 		std::cout<<"shading for light: diffuse = "<<diffuse_intensity<<"; specular = "<<specular_intensity<<std::endl<<"final color "<<color<<std::endl;
 	}
+	
 	return color;
 }
